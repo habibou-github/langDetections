a4bfe0b8c41e1a88f3e45a8956cd48d5516cd868
infoh <info.habibou@gmail.com>
2022-05-31 15:02:53 +0100

    cleaning

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 from flask import Flask, render_template, url_for, request
 from langdetect import detect
+all_language_codes = {
+      "en": "English",
+      "fr": "French",
+      "ar": "Arabic",
+      "de": "Germane",
+      "ru": "Russian"
+}
 
 app = Flask(__name__)
 
@@ -16,10 +23,16 @@ def home():
 def result():
     output = request.form.to_dict()
     print(output)
-    name = detect(output["name"])
+    out = output["name"]
+    name = ''
+    if out:
+        lang = detect(out)
+        name = all_language_codes.get(lang)
 
 
-    return render_template('index.html', name = name)
+
+
+    return render_template('index.html', name = name, out = out)
     
 
 
